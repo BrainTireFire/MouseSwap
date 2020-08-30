e8037613dac386ff7dcd259c736723d5a8291e44
Maciej Kawęcki <skimiskim12@gmail.com>
2020-08-30 11:08:56 +0200

    First commit

---
+++ MouseSwap/MouseButtonSwap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace MouseSwap
+{
+    class MouseButtonSwap
+    {
+        [DllImport("user32.dll")]
+        public static extern Int32 SwapMouseButton(Int32 bSwap);
+
+        //Primary button is Right Click
+        private int primaryButton = SwapMouseButton(1);
+     
+        public void changePrimaryMouseButton()
+        {
+            if (primaryButton==1)
+            {
+                //Primary button is Left Click
+                primaryButton = SwapMouseButton(0);
+
+            }else if(primaryButton == 0)
+            {
+                //Primary button is Right Click
+                primaryButton = SwapMouseButton(1);
+            }
+        }
+
+    }
+}
---
+++ MouseSwap/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using MouseSwap;
+
+class SwapMouse
+{
+    static void Main(string[] args)
+    {
+        MouseButtonSwap mouseButtonSwap = new MouseButtonSwap();
+        mouseButtonSwap.changePrimaryMouseButton();
+
+    }
+}
